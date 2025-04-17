4ee02f56caf326acd5ed59ab1f6d2ad574e7f839
vivicm <vivicm101@gmail.com>
2025-04-17 19:07:07 -0300

    exercicios em C para a próxima aula de lógica de programaçaõ

---
+++ exercicio2.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+    char nome[20];
+    char sobrenome[20];
+
+    printf("digite o seu primeiro nome: ");
+    scanf("%s", nome);
+
+    printf("digite um dos seus sobrenomes: ");
+    scanf("%s", sobrenome);
+
+
+  printf("%s %s", nome, sobrenome);
+
+
+
+
+    return 0;
+
+}
---
+++ lista_exercicios_1/exercicio1.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+
+    printf("Viviane");
+
+
+    return 0;
+}
