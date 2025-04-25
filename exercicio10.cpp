27dbc3fb9265f3d5d79848d4f41d685f6971fa8f
vivicm <vivicm101@gmail.com>
2025-04-24 22:57:08 -0300

    inicio do ex 10

---
+++ exercicio10.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+//10. Faça um programa que, dados 3 números inteiros, mostre na tela o maior deles.
+
+int main() {
+    int v1 = 1;
+    int v2 = 2;
+    int v3 = 3;
+    
+    if (v1 > v2)
+    {
+        printf(" 1 e maior que 2");
+    }
+    else{
+        printf("1 e menor que 2");
+    }
+    
+
+
+    return 0;
+}
